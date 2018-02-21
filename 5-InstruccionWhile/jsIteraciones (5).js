20918cee69f6b21f26e73f327f0112488371ce83
matirue <36230714+matirue@users.noreply.github.com>
2018-02-21 10:15:45 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -1,10 +1,14 @@
 function Mostrar()
 {
 
-var sexo = prompt("ingrese f ó m .");
+var sexo = prompt("Ingrese sexo f ó m .");
+sexo=sexo.toLowerCase();
 
-
-
+while (sexo!='f' && sexo!='m')
+{
+    sexo=prompt("Error. Reingrese sexo f o m. ");
+    sexo=sexo.toLowerCase();
+}
 document.getElementById('Sexo').value=sexo;
 
 }//FIN DE LA FUNCIÓN
