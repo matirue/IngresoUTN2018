1f193fb62ab36233c59f3ae421738f7885ed1c9e
matirue <36230714+matirue@users.noreply.github.com>
2018-02-21 17:57:50 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,25 @@ function Mostrar()
 //tomo el mes 
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch(mesDelAño)
+{
+    
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+    case "Junio":
+    alert("Falta para el invierno.")
+    break;
+
+    case "Julio":
+    case "Agosto":
+    alert("Abrigate que hace frio.")
+    break;
+
+    default:
+    alert("Ya pasamos el frio, ahora calor!!!.")
+    break;
+}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,6 +4,29 @@ function Mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
+switch(mesDelAño)
+{
+    case "Febrero":
+    alert("Si, tiene 28 días.")
+    break;
+
+    case "Abril":
+    
+    case "Mayo":
+    
+    case "Junio":
+    
+    case "Septiembre":
+
+    case "Noviembre":
+    alert("Si, tiene 30 dias.")
+    break;
+
+    default:
+    alert("Si, tiene 31 días")
+    break;
+    
+}
 	
 	
 
