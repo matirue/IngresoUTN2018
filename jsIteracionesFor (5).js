a135d78db2aa27a84af48a2d01f6a39deb426aad
matirue <36230714+matirue@users.noreply.github.com>
2018-02-22 12:04:56 -0300

    Add files via upload

---
+++ jsIteracionesFor (5).js
@@ -0,0 +1,17 @@
+function Mostrar()
+{
+
+    for(var i=0;  ; i++)
+{
+    var respuesta=parseInt(prompt("Ingrese un numero"));
+
+    if(i==9)
+    {
+        break;
+    }
+    
+}
+alert("sadasd");
+
+
+}//FIN DE LA FUNCIÓN
