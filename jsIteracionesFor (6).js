939640836a9e8461cb955ae2a2220540ce263a4c
matirue <36230714+matirue@users.noreply.github.com>
2018-02-22 12:30:17 -0300

    Add files via upload

---
+++ jsIteracionesFor (6).js
@@ -0,0 +1,30 @@
+function Mostrar()
+{
+    var pares=1;
+    var cantPar;
+    var num=parseInt(prompt("Ingrese un numero: "));
+
+
+    for(i=0;i==num ;i++)
+    {
+        
+
+        if(num%2==1)
+        {
+            cantPar++;
+            continue;
+        }
+
+        
+
+
+
+    }
+
+   console.log("los numeros pares son: " +pares);
+   console.log("La cantidad de numeros pares es: " +cantPar);
+
+
+
+
+}//FIN DE LA FUNCIÓN
