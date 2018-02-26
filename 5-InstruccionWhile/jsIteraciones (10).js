939c3ba4be4c439ab3e41d583e148165a1fa6af5
matirue <36230714+matirue@users.noreply.github.com>
2018-02-26 16:46:05 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -3,6 +3,7 @@ function Mostrar()
 
 	var contador=0;
 	//declarar contadores y variables 
+	
 	var sumaNeg=0;
 	var sumaPos=0;
 	var cantPos=0;
@@ -13,40 +14,37 @@ function Mostrar()
 	var promNeg;
 	var diferencia;
 	var num;
+
 	var respuesta;
 
 	do
 	{
 		num=parseInt(prompt("Ingrese un numero: "));
 		contador++;
-		
+
 		if(num<0)
 		{
 			sumaNeg-=num;
 			cantNeg++;
 		}
-
-		else if (num>0)
+		else if(num>0)
 		{
 			sumaPos+=num;
 			cantPos++;
 		}
-
 		else
 		{
 			cantCeros++;
 		}
 
-		if(num%2==0)
+		if (num%2==0)
 		{
 			cantPar++;
 		}
+
 		
-
-
-	  
 	respuesta=prompt("¿Desea continuar? ");
-	}while(respuesta=='s')
+	}while (respuesta=='s')
 
 	promNeg=sumaNeg/cantNeg;
 
@@ -54,17 +52,16 @@ function Mostrar()
 
 	diferencia=sumaPos-sumaNeg;
 
-	
+	document.write("1-Suma de los negativos. " +sumaNeg+ "<br/>");
+	document.write("2-Suma de los positivos. " +sumaPos+ "<br/>");
+	document.write("3-Cantidad de positivos. " +cantPos+ "<br/>");
+	document.write("4-Cantidad de negativos. " +cantNeg+ "<br/>");
+	document.write("5-Cantidad de ceros.  " +cantCeros+ "<br/>");
+	document.write("6-Cantidad de números pares. " +cantPar+ "<br/>");
+	document.write("7-Promedio de positivos. " +promPos+ "<br/>");
+	document.write("8-Promedios de negativos. " +promNeg+ "<br/>");
+	document.write("Diferencia entre positivos y negativos, (positvos-negativos). " +diferencia+ "<br/>");
 
-	document.write("1-La suma de los negativos es de " +sumaNeg+ "<br/>");
-	document.write("2-La suma de los positivos es de " +sumaPos+ "<br/>");
-	document.write("3-La cantidad de positivos es de " +cantPos+ "<br/>");
-	document.write("4-La cantidad de numeros negativos es de " +cantNeg+ "<br/>");
-	document.write("5-La cantidad de ceros es de " +cantCeros+ "<br/>");
-	document.write("6-La cantidad de numeros pares es de " +cantPar+ "<br/>");
-	document.write("7-El promedio de los positivos es de " +promPos+ "<br/>");
-	document.write("8-El promedio de los negativos es de " +promNeg+ "<br/>");
-	document.write("9-La diferencias entre positivos y negativos es de " +diferencia+ "<br/>");
 
 
 
