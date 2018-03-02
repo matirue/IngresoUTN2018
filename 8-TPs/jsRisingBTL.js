1714b98a3d2b5318bae531288e70ff1cc5b59ff8
matirue <36230714+matirue@users.noreply.github.com>
2018-03-02 20:28:13 -0300

    Add files via upload

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -29,11 +29,93 @@ function ComenzarIngreso ()
            {
               sexo=prompt("Error. Reingrese su sexo, 'F' para femenino y 'M' para masculino. ").toUpperCase();
            }
+
+          if(sexo=='F')
+          {
+              sexo="Femenino";
+          }
+          else
+          {
+              sexo="Masculino";
+          }
         document.getElementById("Sexo").value=sexo;
     }
 
     {
-        var estadoCivil=prompt("Ingrese su estado civil")
+        var estadoCivil=prompt("Ingrese su Estado Civil (Soltero/Casado/Divorciado/Viudo).");
+
+        while(!(estadoCivil=='Soltero' || estadoCivil=='Casado' || estadoCivil=='Divorciado' || estadoCivil=='Viudo'))
+        {
+            estadoCivil=prompt("Error. Rengrese su Estado Civil (Soltero/Casado/Divorciado/Viudo).");
+        }
+        switch(estadoCivil)
+        {
+            case 1:
+             estadoCivil="Soltero";
+             break;
+            
+            case 2:
+             estadoCivil="Casado";
+             break;
+            
+            case 4:
+             estadoCivil="Divorciado";
+             break;
+
+            case 5:
+             estadoCivil="Viudo";
+             break;
+        }
+
+        document.getElementById("EstadoCivil").value=estadoCivil;
+    }
+
+    {
+        var sueldo=parseInt(prompt("Monto de sueldo $"));
+
+        while(sueldo<8000 || sueldo!=parseInt(sueldo))
+         {
+            sueldo=parseInt(prompt("El sueldo a ingresar debe ser mayor a $8000"));
+         }
+        
+        document.getElementById("Sueldo").value="$"+sueldo;
+    }
+
+    {
+        var numLegajo=parseInt(prompt("Ingrese su mumero de legajo, no mas de 4 cifras y sin ceros a la izquierda: "));
+
+        while(!(numLegajo>=1000 && numLegajo<10000 && numLegajo==parseInt(numLegajo)))
+         {
+             numLegajo=parseInt(prompt("Error. Por favor reingrese numero de legajo, no mas de 4 cifras y sin ceros a la izquierda: "));
+         }
+
+        document.getElementById("Legajo").value=numLegajo;
+    }
+
+    {
+        var nacion=prompt("Ingrese su nacionalidad (A: Argentino. E: Extranjero. N: No nacionalizado): ").toUpperCase();
+
+        while(!(nacion=='A' || nacion=='E' || nacion=='N'))
+         {
+             nacion=prompt("Error. Reingrese su nacionalidad (A: Argentino. E: Extranjero. N: No nacionalizado): ").toUpperCase();
+         }
+        
+         switch(nacion)
+          {
+              case 'A':
+               nacion="Argentino.";
+               break;
+
+              case 'E':
+               nacion="Extranjero.";
+               break;
+
+              case 'N':
+               nacion="No nacionalizado.";
+               break;
+          }
+
+        document.getElementById("Nacionalidad").value=nacion;
     }
 
 
